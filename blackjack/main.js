a4e4ecd61d09e386e10e6fa09b94c96c974cc851
BrianZ26 <brian.zhao026@gmail.com>
2023-02-13 14:01:22 -0500

    a

---
+++ blackjack/main.js
@@ -0,0 +1,28 @@
+function deckBuilder() {
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
+  const suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
+  const cards = [];
+  for (let s = 0; s < suits.length; s++) {
+    for (let v = 0; v < values.length; v++) {
+      const value = values[v];
+      const suit = suits[s];
+      cards.push({ value, suit });
+    }
+  }
+  return cards;
+}
+console.log(deckBuilder());
