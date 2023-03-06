0334fe7b713c08778603acbef7160725fb7d9345
BrianZ26 <brian.zhao026@gmail.com>
2023-03-06 18:48:10 -0500

    A

--- blackjack/main.js
+++ blackjack/main.js
@@ -15,28 +15,14 @@ function deckBuilder() {
     "K",
   ];
   let suits = ["♦", "♣", "♥", "♠"];
-  let score = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
   let cards = [];
   for (let s = 0; s < suits.length; s++) {
     for (let v = 0; v < values.length; v++) {
       const value = values[v];
       const suit = suits[s];
-      const sc = score[v];
-      cards.push({ value, suit, sc });
+      cards.push({ value, suit });
     }
   }
   return cards;
 }
 const cards = deckBuilder();
-dom.play.addEventListener("click", function play() {
-  hand = [];
-  totalScore = [];
-  playerDraw(cards);
-  playerDraw(cards);
-  let sum = 0;
-  totalScore.forEach((el) => {
-    sum += el;
-  });
-  console.log(hand, sum);
-});
-deckBuilder(cards);
