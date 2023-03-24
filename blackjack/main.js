97dc5832d3794c3afa0595ae2cebc2d92096a317
BrianZ26 <brian.zhao026@gmail.com>
2023-03-23 21:10:02 -0400

    A

--- blackjack/main.js
+++ blackjack/main.js
@@ -1,61 +1,61 @@
 const DOM = {
   playercards: document.getElementById("player-hand"),
   dealercards: document.getElementById("dealer-hand"),
+  playcards: document.getElementById("play"),
 };
-const values = [
-  "A",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K",
-];
-
-let suits = ["♦", "♣", "♥", "♠"];
+const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+const suits = ["♦", "♣", "♥", "♠"];
 
 let dealerHand = [];
 
 let playerHand = [];
-
-const deckBuilder = () => {
-  let deck = [];
+let deck = [];
+function deckBuilder() {
   suits.forEach((suit) => {
     values.forEach((value) => {
-      const card = value + suit;
+      const card = [value + suit];
       deck.push(card);
     });
   });
   return deck;
-};
-
-const drawRandomCard = () => {
+}
+function drawRandomCard() {
   const cardNum = Math.floor(Math.random() * deck.length);
   const card = deck[cardNum];
   deck.splice(cardNum, 1);
-  console.log(card);
   return card;
-};
-
-const dealhands = () => {
+}
+function calcValue() {
+  let value = 0;
+  playerHand.forEach((card) => {
+    if (card[0] === "K" || card[0] === "Q" || card[0] === "J") {
+      value += 10;
+    } else value += card[0];
+  });
+}
+function playerHit() {}
+function dealhands() {
   const dealerHand = [drawRandomCard(), drawRandomCard()];
+  calcValue(dealerHand);
   dealerHand.forEach((card) => {
     DOM.dealercards.insertAdjacentHTML(
-      "afterend"`<div class="cards">${card}</div>`
+      "beforeend",
+      `<div class="cards">${card}</div>`
     );
   });
   const playerHand = [drawRandomCard(), drawRandomCard()];
+  calcValue(playerHand);
+  console.log(playerHand);
+  let pScore = 0;
   playerHand.forEach((card) => {
+    console.log(pScore);
     DOM.playercards.insertAdjacentHTML(
-      "afterend"`<div class="cards">${card}</div>`
+      "beforeend",
+      `<div class="cards">${card}</div>`
     );
   });
-};
-dealhands;
-console.log(deck);
+}
+DOM.playcards.addEventListener("click", function playgame() {
+  deckBuilder();
+  dealhands();
+});
